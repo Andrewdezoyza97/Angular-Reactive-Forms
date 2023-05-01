832c30f77f9961373db4510b6dbb4e1ba6df6611
Andrew De Zoyza <andrewdezoyza@aizenit.com>
2023-05-02 03:26:42 +0530

    Displaying Validation Message

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -34,6 +34,12 @@ function ratingRange(min: number, max: number): ValidatorFn {
 export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
   customer = new Customer();
+  emailMessage = '';
+
+  private validationMessages :any = {
+    required: 'Please Enter Your Email Address',
+    email: 'Please Enter a Valid Email Address'
+  };
 
   constructor(private fb: FormBuilder) { }
 
@@ -44,7 +50,7 @@ export class CustomerComponent implements OnInit {
       emailGroup: this.fb.group({
         email: ['', [Validators.required, Validators.email]],
         confirmEmail: ['', Validators.required],
-      },{Validators : emailMatcher}),
+      }, { Validators: emailMatcher }),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
@@ -53,6 +59,11 @@ export class CustomerComponent implements OnInit {
 
     this.customerForm.get('notification')?.valueChanges.subscribe(
       value => this.setNotification(value)
+    );
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl?.valueChanges.subscribe(
+      value => this.setMessage(emailControl)
     );
   }
 
@@ -67,6 +78,14 @@ export class CustomerComponent implements OnInit {
   save(): void {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
   }
 
   setNotification(notifyVia: string): void {
